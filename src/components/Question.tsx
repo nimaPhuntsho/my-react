86e5a147bf8a26bb76fcd630dd6704550a386d6a
nimaPhuntsho <nimafunso@gmail.com>
2024-04-01 23:49:23 +1000

    quiz app set up

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -3,9 +3,11 @@ import {
   Button,
   Card,
   CardBody,
+  Flex,
   Heading,
   HStack,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { questions } from "../data/questions";
@@ -29,20 +31,37 @@ export interface Question {
 const Question = () => {
   //local state
   const [selectedAnswer, setSelectedAnswer] = useState<string>("");
-  const [userAnswer, setUserAnswer] = useState<QuizAnswer[]>([]);
+  const [quizQuestions, setQuizQuestions] = useState<QuizAnswer[]>([]);
 
   //global state
   const quizStore = useQuiz().items;
   const addQuizAnswer = useQuiz().add;
 
-  const getUserAnswer = (answer: string, question: Question) => {
+  useEffect(() => {
+    const q = newQuestions();
+    setQuizQuestions(q);
+  }, []);
+
+  const newQuestions = (): QuizAnswer[] => {
+    return questions.map((element) => {
+      const temp: QuizAnswer = {
+        ...element,
+        hasAnswered: false,
+        userAnswer: "",
+      };
+      return temp;
+    });
+  };
+
+  const getUserAnswer = (answer: string, question: QuizAnswer) => {
     setSelectedAnswer(answer);
-    const temp: QuizAnswer = {
-      ...question,
-      hasAnswered: true,
-      userAnswer: answer,
-    };
-    setUserAnswer([...userAnswer, temp]);
+    const current = quizQuestions.map((element) => {
+      if (element.id === question.id) {
+        return { ...element, hasAnswered: true, userAnswer: answer };
+      }
+      return element;
+    });
+    setQuizQuestions(current);
   };
 
   const checkAnswer = (userAnswer: string, correctAnswer: string): number => {
@@ -55,26 +74,32 @@ const Question = () => {
     return result;
   };
 
-  const submit = (question: Question) => {
-    const { correctAnswer } = question;
-    const current: QuizAnswer = {
-      ...question,
-      userAnswer: selectedAnswer,
-      //   point: checkAnswer(selectedAnswer, correctAnswer),
-      hasAnswered: true,
-    };
-    addQuizAnswer(current);
-    console.log(userAnswer);
+  const submit = (question: QuizAnswer) => {
+    // const { correctAnswer } = question;
+    // const current: QuizAnswer = {
+    //   ...question,
+    //   userAnswer: selectedAnswer,
+    //   //   point: checkAnswer(selectedAnswer, correctAnswer),
+    //   hasAnswered: true,
+    // };
+    addQuizAnswer(question);
+    const current = quizQuestions.map((element) => {
+      if (element.id === question.id) {
+        return { ...element, hasAnswered: false };
+      }
+      return element;
+    });
+    setQuizQuestions(current);
   };
 
-  const renderQuestions = questions.map((element, index) => (
-    <Card key={element.id} width={"700px"}>
+  const renderQuestions = quizQuestions.map((element, index) => (
+    <Card key={element.id} width={"500px"}>
       <CardBody>
-        <HStack alignItems={"center"}>
+        <HStack alignItems={"start"}>
           <Heading padding={0} margin={0} size={"md"}>
             {element.id}
           </Heading>
-          <Text padding={0} margin={0}>
+          <Text fontWeight={500} padding={0} margin={0}>
             {element.question}
           </Text>
         </HStack>
@@ -85,15 +110,25 @@ const Question = () => {
               padding="1rem"
               borderRadius={"4px"}
               key={index}
-              _hover={{ cursor: "pointer" }}
+              _hover={{ border: "1px solid red", cursor: "pointer" }}
               onClick={() => getUserAnswer(answer, element)}
+              backgroundColor={
+                selectedAnswer === answer.trim() &&
+                element.userAnswer === answer.trim()
+                  ? "rgb(246, 148, 148)"
+                  : "transparent"
+              }
             >
               {answer}
             </Text>
           ))}
         </Box>
         <HStack>
-          <Button onClick={() => submit(element)} colorScheme={"twitter"}>
+          <Button
+            isDisabled={!element.hasAnswered}
+            onClick={() => submit(element)}
+            colorScheme={"twitter"}
+          >
             Submit
           </Button>
         </HStack>
@@ -102,18 +137,24 @@ const Question = () => {
   ));
   return (
     <>
-      <Heading size={"lg"}>Quiz</Heading>
-      <div className="quiz">
-        <Box className={styles["question-wrapper"]}>{renderQuestions}</Box>
-        <div>
-          {quizStore.map((element) => (
-            <Box border={"1px solid"} key={element.id}>
-              <li> U- {element.userAnswer} </li>
-              <li> Correct - {element.correctAnswer} </li>
-              <li> {element.question} </li>
-            </Box>
-          ))}
-        </div>
+      <Flex direction={"column"}>
+        <Box className={styles["question-wrapper"]}>
+          <Heading size={"lg"}>Quiz</Heading>
+          {renderQuestions}
+        </Box>
+      </Flex>
+
+      {/* <div className="quiz">
+      </div> */}
+
+      <div>
+        {quizStore.map((element) => (
+          <Box border={"1px solid"} key={element.id}>
+            <li> U- {element.userAnswer} </li>
+            <li> Correct - {element.correctAnswer} </li>
+            <li> {element.question} </li>
+          </Box>
+        ))}
       </div>
     </>
   );
